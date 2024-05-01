a0913112dd71fdce3fed6936beb9c835956ed579
PrismGAFA <Markjinli@163.com>
2024-05-02 00:24:39 +0800

    更新

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchLaptops() {
     try {
-        const response = await fetch('https://markjinli.github.io/PrismGAFA/data/Laptop_Data_UTF8.csv');
+        const response = await fetch('https://markjinli.github.io/PrismGAFA/Laptop_Data_UTF8.csv');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -22,38 +22,18 @@ function displayLaptops(laptops) {
     const tableBody = document.getElementById('laptop-table').getElementsByTagName('tbody')[0];
     laptops.forEach(laptop => {
         const row = tableBody.insertRow();
-        const imgCell = row.insertCell();
-        const img = document.createElement('img');
-        img.src = 'https://markjinli.github.io/PrismGAFA/images/Y7000.JPG'; // 图片链接现在直接从CSV的第一列读取
-        img.style.width = '100px';
-        imgCell.appendChild(img);
-
-        laptop.slice(1).forEach(text => { // 从第二列开始读取其余数据
+        laptop.forEach((cellData, index) => {
             const cell = row.insertCell();
-            cell.textContent = text;
+            if (index === 0) { // 第一列是图片URL
+                const img = document.createElement('img');
+                img.src = cellData;
+                img.alt = 'Laptop Image';
+                cell.appendChild(img);
+            } else {
+                cell.textContent = cellData;
+            }
         });
     });
-}
-
-function filterLaptops() {
-    const minPrice = parseFloat(document.getElementById('minPrice').value);
-    const maxPrice = parseFloat(document.getElementById('maxPrice').value);
-    const allRows = document.getElementById('laptop-table').getElementsByTagName('tbody')[0].rows;
-    Array.from(allRows).forEach(row => {
-        const price = parseFloat(row.cells[2].textContent);
-        if (price < minPrice || price > maxPrice) {
-            row.style.display = 'none';
-        } else {
-            row.style.display = '';
-        }
-    });
-}
-
-function updatePriceRange() {
-    const minPriceValue = document.getElementById('minPrice').value;
-    const maxPriceValue = document.getElementById('maxPrice').value;
-    document.getElementById('minPriceValue').textContent = minPriceValue;
-    document.getElementById('maxPriceValue').textContent = maxPriceValue;
 }
 
 window.onload = fetchLaptops;
